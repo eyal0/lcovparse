e949525117851cbbb4a5a1cdc272bcbb9a8a64b4
eyal0 <109809+eyal0@users.noreply.github.com>
2020-01-06 13:26:47 -0500

    Add unit tests for lcovparse

---
+++ tests/context.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+"""Context for all tests."""
+
+from __future__ import absolute_import
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)) + "../lcovparse"))
+
+import lcovparse # pylint: disable=wrong-import-position,unused-import
---
+++ tests/test_lcovparse.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+"""Test coverage.py"""
+
+from __future__ import division
+import os
+import unittest
+import json
+import difflib
+
+from .context import lcovparse
+from lcovparse import lcovparse
+
+class LcovparseTests(unittest.TestCase):
+    """Test lcovparse."""
+    def compare_lines(self, actual_lines, expected_lines, filename):
+        """Helper for comparing lines.
+
+        Outputs a unified diff.
+        """
+        self.assertEqual(actual_lines,
+                         expected_lines,
+                         "\n\n" + "\n".join(
+                             difflib.unified_diff(
+                                 expected_lines,
+                                 actual_lines,
+                                 os.path.relpath(filename),
+                                 os.path.relpath(filename),
+                                 lineterm="")))
+
+    def test_from_lcov(self):
+        """Test Coverage.from_lcov(file)."""
+        path = os.path.dirname(os.path.realpath(__file__))
+        with open(os.path.join(path, "branch-lcov.info")) as lcov_file:
+          result = lcovparse(lcov_file.read())
+        expected_file = os.path.join(path, "branch-lcov.info.json")
+        with open(expected_file) as expected:
+            expected_lines = expected.read().splitlines() # No newlines
+            actual_lines = json.dumps(result, indent=2, sort_keys=True).splitlines()
+            self.compare_lines(actual_lines, expected_lines, expected_file)
+
+if __name__ == '__main__':
+    unittest.main()
